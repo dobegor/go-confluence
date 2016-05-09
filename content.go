64be0db6586778aae0b2d22aef7f409808e8283c
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2016-05-09 14:04:52 +0100

    Change api url

--- content.go
+++ content.go
@@ -7,19 +7,98 @@ import (
 	"strings"
 )
 
+// type Content struct {
+// 	Id     string `json:"id"`
+// 	Type   string `json:"type"`
+// 	Status string `json:"status"`
+// 	Title  string `json:"title"`
+// 	Body   struct {
+// 		Storage struct {
+// 			Value          string `json:"value"`
+// 			Representation string `json:"representation"`
+// 		} `json:"storage"`
+// 	} `json:"body"`
+// 	Version struct {
+// 		Number int `json:"number"`
+// 	} `json:"version"`
+// }
+
 type Content struct {
-	Id     string `json:"id"`
-	Type   string `json:"type"`
-	Status string `json:"status"`
-	Title  string `json:"title"`
-	Body   struct {
-		Storage struct {
-			Value          string `json:"value"`
+	Expandable struct {
+		Ancestors   string `json:"ancestors"`
+		Children    string `json:"children"`
+		Descendants string `json:"descendants"`
+		History     string `json:"history"`
+		Metadata    string `json:"metadata"`
+	} `json:"_expandable"`
+	Links struct {
+		Base       string `json:"base"`
+		Collection string `json:"collection"`
+		Self       string `json:"self"`
+		Tinyui     string `json:"tinyui"`
+		Webui      string `json:"webui"`
+	} `json:"_links"`
+	Body struct {
+		Expandable struct {
+			Editor     string `json:"editor"`
+			ExportView string `json:"export_view"`
+			Storage    string `json:"storage"`
+		} `json:"_expandable"`
+		View struct {
+			Expandable struct {
+				Content string `json:"content"`
+			} `json:"_expandable"`
 			Representation string `json:"representation"`
-		} `json:"storage"`
+			Value          string `json:"value"`
+		} `json:"view"`
 	} `json:"body"`
+	Container struct {
+		Expandable struct {
+			Description string `json:"description"`
+			Homepage    string `json:"homepage"`
+			Icon        string `json:"icon"`
+		} `json:"_expandable"`
+		Links struct {
+			Self string `json:"self"`
+		} `json:"_links"`
+		ID   int    `json:"id"`
+		Key  string `json:"key"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+	} `json:"container"`
+	ID    string `json:"id"`
+	Space struct {
+		Expandable struct {
+			Description string `json:"description"`
+			Homepage    string `json:"homepage"`
+			Icon        string `json:"icon"`
+		} `json:"_expandable"`
+		Links struct {
+			Self string `json:"self"`
+		} `json:"_links"`
+		ID   int    `json:"id"`
+		Key  string `json:"key"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+	} `json:"space"`
+	Title   string `json:"title"`
+	Type    string `json:"type"`
 	Version struct {
-		Number int `json:"number"`
+		By struct {
+			DisplayName    string `json:"displayName"`
+			ProfilePicture struct {
+				Height    int    `json:"height"`
+				IsDefault bool   `json:"isDefault"`
+				Path      string `json:"path"`
+				Width     int    `json:"width"`
+			} `json:"profilePicture"`
+			Type     string `json:"type"`
+			Username string `json:"username"`
+		} `json:"by"`
+		Message   string `json:"message"`
+		MinorEdit bool   `json:"minorEdit"`
+		Number    int    `json:"number"`
+		When      string `json:"when"`
 	} `json:"version"`
 }
 
