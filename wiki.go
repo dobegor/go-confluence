644169646bdf3c60d11ce36e6b9bc5097a067150
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2016-05-10 10:49:16 +0100

    Debug info

--- wiki.go
+++ wiki.go
@@ -67,6 +67,10 @@ func (w *Wiki) sendRequest(req *http.Request) ([]byte, error) {
 	req.Header.Add("Accept", "application/json, */*")
 	w.authMethod.auth(req)
 
+	fmt.Println(strings(req.Body))
+	fmt.Println(req.Method)
+	fmt.Println(req.URL.Path)
+
 	resp, err := w.client.Do(req)
 	if err != nil {
 		return nil, err
